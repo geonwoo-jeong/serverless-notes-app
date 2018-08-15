24061f5e5d27c185172c525322e693b982709b37
mlc_jeong <geonwoo.jeong@mizuiwa.co.jp>
2018-08-15 14:51:28 +0900

    Add get note api

--- get.js
+++ get.js
@@ -1,1 +1,31 @@
-import * as dynamoDbLib from "./libs"
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import {
+  success,
+  failure
+} from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const params = {
+    TableName: "notes",
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id,
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      callback(null, success(result.Item));
+    } else {
+      callback(null, failure({
+        status: 404,
+        error: "Item not found.",
+      }))
+    }
+  } catch (e) {
+    callback(null, failure({
+      status: 500,
+    }))
+  }
+}
